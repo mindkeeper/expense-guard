8ee930bb07af743d3c99622ab7851992a1b362cd
unknown <fcb.nyak@gmail.com>
2024-09-17 21:26:43 +0700

    feat: show swagger to production

--- src/main.ts
+++ src/main.ts
@@ -33,24 +33,41 @@ async function bootstrap() {
   app.useGlobalInterceptors(new TransformResponseInterceptor(reflector));
 
   // swagger
-  if (configService.get('NODE_ENV') !== 'production') {
-    const theme = new SwaggerTheme();
-    const config = new DocumentBuilder()
-      .setTitle('coffeshop')
-      .setDescription('coffeshop API documentation')
-      .setVersion('0.1')
-      .addBearerAuth()
-      .build();
-    patchNestjsSwagger();
-    const document = SwaggerModule.createDocument(app, config);
-    SwaggerModule.setup('swagger', app, document, {
-      useGlobalPrefix: false,
-      customCss: theme.getBuffer(SwaggerThemeNameEnum.ONE_DARK),
-      swaggerOptions: {
-        persistAuthorization: true,
-      },
-    });
-  }
+  const theme = new SwaggerTheme();
+  const config = new DocumentBuilder()
+    .setTitle('coffeshop')
+    .setDescription('coffeshop API documentation')
+    .setVersion('0.1')
+    .addBearerAuth()
+    .build();
+  patchNestjsSwagger();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('swagger', app, document, {
+    useGlobalPrefix: false,
+    customCss: theme.getBuffer(SwaggerThemeNameEnum.ONE_DARK),
+    swaggerOptions: {
+      persistAuthorization: true,
+    },
+  });
+
+  // if (configService.get('NODE_ENV') !== 'production') {
+  //   const theme = new SwaggerTheme();
+  //   const config = new DocumentBuilder()
+  //     .setTitle('coffeshop')
+  //     .setDescription('coffeshop API documentation')
+  //     .setVersion('0.1')
+  //     .addBearerAuth()
+  //     .build();
+  //   patchNestjsSwagger();
+  //   const document = SwaggerModule.createDocument(app, config);
+  //   SwaggerModule.setup('swagger', app, document, {
+  //     useGlobalPrefix: false,
+  //     customCss: theme.getBuffer(SwaggerThemeNameEnum.ONE_DARK),
+  //     swaggerOptions: {
+  //       persistAuthorization: true,
+  //     },
+  //   });
+  // }
   await app.listen(8080);
 }
 bootstrap();
